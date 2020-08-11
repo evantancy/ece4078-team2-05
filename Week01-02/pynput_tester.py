bfcbef07b261ee65f1645f5b10836e95184f8ecc
Evan Tan <tcyevan@gmail.com>
2020-08-11 18:13:49 +1000

    Revert keyboard input tester to default

--- Week01-02/pynput_tester.py
+++ Week01-02/pynput_tester.py
@@ -1,6 +1,6 @@
 from pynput import keyboard
 
-def on_press(key,var):
+def on_press(key):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
@@ -17,12 +17,12 @@ def on_release(key):
 
 # Collect events until released
 with keyboard.Listener(
-        on_press=on_press(key,var),
+        on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
-    on_press=on_press(key,var),
+    on_press=on_press,
     on_release=on_release)
 listener.start()
