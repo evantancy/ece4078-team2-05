45f76782bd16c6606ce414463d26a7b527ac87cb
Evan Tan <tcyevan@gmail.com>
2020-08-13 00:05:19 +1000

    Added ability to increment speeds

--- Week01-02/keyboardControlStarter.py
+++ Week01-02/keyboardControlStarter.py
@@ -6,16 +6,17 @@ import penguinPiC
 class Keyboard:
     
     
-    def __init__(self, ppi=None, fwd=100, rot=40):
+    
+    def __init__(self, ppi=None, forward_vel=100, turning_vel=40):
         """Constructor
         Args:
-            ppi (penguinPiC): penguinPiC object. Defaults to None.
+            ppi (penguinPiC): penguinPiC Object. Defaults to None.
+            forward_vel (int): Forward/backwards velocity Defaults to 100.
+            turning_vel (int): Turning velocity Defaults to 40.
         """
         
         # Triggers for key presses
         self.directions = [False for _ in range(4)]
-        # Triggers for incrementing speeds
-        self.increments = self.directions
         self.signal_stop = False
         
         # Connection to PenguinPi robot
@@ -23,30 +24,64 @@ class Keyboard:
         
         # [Left, Right]
         self.wheel_vels = [0, 0]
-        self.wheel_vel_forward = fwd
-        self.wheel_vel_rotation = rot
+        self.wheel_vel_forward = forward_vel
+        self.wheel_vel_turning = turning_vel
         
-        # Initialize pynput keyboard listener
+        # Initialize pynput keyboard listener (non-blocking)
         self.listener = Listener(on_press=self.on_press, on_release=self.on_release).start()
         
         
-    def set_state(self, key, activate):
-        """What we want the robot to do
+        
+    def trigger_state(self, key, activate):
+        """Set different states of robot's motion
         Args:
             key (Key): keyboard input
             activate (bool): turn state on/off
         """
         
-        if str(key.char).lower() == 'w':
-            self.directions[0] = activate
-        elif str(key.char).lower() == 's':
-            self.directions[1] = activate
-        elif str(key.char).lower() == 'a':
-            self.directions[2] = activate
-        elif str(key.char).lower() == 'd':
-            self.directions[3] = activate
-        elif key == Key.space:
-            self.signal_stop = activate
+        try:
+            if str(key.char).lower() == 'w':
+                self.directions[0] = activate
+                
+            elif str(key.char).lower() == 's':
+                self.directions[1] = activate
+                
+            elif str(key.char).lower() == 'a':
+                self.directions[2] = activate
+                
+            elif str(key.char).lower() == 'd':
+                self.directions[3] = activate
+                
+            elif key == Key.space:
+                self.signal_stop = activate
+                
+        except AttributeError:
+            pass
+    
+    
+    
+    def set_target(self, key):
+        """Increase/decrease target velocities
+        Args:
+            key (Key): keyboard input
+        """
+        
+        if key == Key.up:
+            if self.wheel_vel_forward < 300:
+                self.wheel_vel_forward += 5
+            
+        elif key == Key.down:
+            if self.wheel_vel_forward > 0:
+                self.wheel_vel_forward -= 5
+            
+        elif key == Key.left:
+            if self.wheel_vel_turning > 0:
+                self.wheel_vel_turning -= 2
+            
+        elif key == Key.right:
+            if self.wheel_vel_turning < 100:
+                self.wheel_vel_turning += 2
+
 
 
     def on_press(self, key):
@@ -54,94 +89,71 @@ class Keyboard:
         Args:
             key (Key): keyboard input
         """
-        try:
-            self.set_state(key, True)
-        except AttributeError:
-            print("Key {0} pressed, use W,A,S,D Keys!".format(
-            key.char))
+        
+        self.trigger_state(key, True)
+        self.set_target(key)
         # Request robot to move
         self.send_drive_signal()
         
     def on_release(self, key):
-        """Function call when keys are release
+        """Function call when keys are released
         Args:
             key (Key): keyboard input
         """
-        try:
-            self.set_state(key, False)
-        except AttributeError:
-            print("Key {0} pressed, use W,A,S,D Keys!".format(
-            key.char))
+        self.trigger_state(key, False)
         
-    def get_drive_signal(self):           
         
-        # compute drive_forward and drive_rotate using wheel_vel_forward and wheel_vel_rotation
-        #drive_forward = 100*(np.multiply(self.directions[0],1)-np.multiply(self.directions[1],1))
-        #drive_rotate = 20*(np.multiply(self.directions[2],1)-np.multiply(self.directions[3],1))
-
-        # translate drive_forward and drive_rotate into left_speed and right_speed
-        #left_speed = drive_forward -(drive_rotate<0)*drive_rotate
-        #right_speed = drive_forward +(drive_rotate>0)*drive_rotate
+    def get_drive_signal(self):
+        """Determine target velocities
+        Returns:
+            left_target: Target velocity for left wheel
+            right_target: Target velocity for right wheel
+        """
         
-        if self.directions[0]:
-            left_speed = self.wheel_vel_forward
-            right_speed = self.wheel_vel_forward
-            self.directions[0] = False
-
-        elif self.directions[1]:
-            left_speed = -self.wheel_vel_forward
-            right_speed = -self.wheel_vel_forward
-            self.directions[1] = False
-
-        elif self.directions[2]:
-            left_speed = 0
-            right_speed = self.wheel_vel_rotation
-            self.directions[2] = False
-
-        elif self.directions[3]:
-            left_speed = self.wheel_vel_rotation
-            right_speed = 0
-            self.directions[3] = False
-
-        elif self.directions[4]:
-            self.wheel_vel_forward+=50
-            self.directions[4] = False
-            if self.wheel_vels[0]==0 and self.wheel_vels[1]==0:
-                left_speed = 0
-                right_speed = 0
-            elif self.wheel_vels[0]>0 and self.wheel_vels[1]>0:
-                left_speed = self.wheel_vel_forward
-                right_speed = self.wheel_vel_forward
-            elif self.wheel_vels[0]<0 and self.wheel_vels[1]<0:
-                left_speed = -self.wheel_vel_forward
-                right_speed = -self.wheel_vel_forward
-            elif self.wheel_vels[0]>self.wheel_vels[1]:
-                left_speed = self.wheel_vel_rotation
-                right_speed = 0
-            else:
-                left_speed = 0
-                right_speed = self.wheel_vel_rotation
-
-            
-
-        elif self.signal_stop:
-            left_speed = 0
-            right_speed = 0
-            self.wheel_vel_forward = 100
-            self.wheel_vel_rotation = 20
-            self.signal_stop = False
-
-        return left_speed, right_speed
+        left_target = 0
+        right_target = 0
+        
+        for index, direction in enumerate(self.directions):
+            if direction == True:
+                if index == 0:
+                    # Forward
+                    left_target += self.wheel_vel_forward
+                    right_target += self.wheel_vel_forward
+                    
+                elif index == 1:
+                    # Backward
+                    left_target += -self.wheel_vel_forward
+                    right_target += -self.wheel_vel_forward
+                    
+                elif index == 2:
+                    # Turn Left
+                    left_target += 0
+                    right_target += self.wheel_vel_turning
+                    
+                elif index == 3:
+                    # Turn Right
+                    left_target += self.wheel_vel_turning
+                    right_target += 0
+                    
+                elif self.signal_stop:
+                    self.wheel_vel_forward = 100
+                    self.wheel_vel_turning = 40
+                    self.signal_stop = False
+                    
+        return left_target, right_target
+    
     
     def send_drive_signal(self):
         if not self.ppi is None:
-            lv, rv = self.get_drive_signal()
-            lv, rv = self.ppi.set_velocity(lv, rv)
-            self.wheel_vels = [lv, rv]
+            l_target, r_target = self.get_drive_signal()
+            self.wheel_vels = self.ppi.set_velocity(l_target, r_target)
             
     def latest_drive_signal(self):
         # Return current state of robot
         return self.wheel_vels
+    
+    def diagnostics(self):
+        lst = [self.wheel_vel_forward, self.wheel_vel_turning]
     
 
 if __name__ == "__main__":
@@ -151,34 +163,33 @@ if __name__ == "__main__":
     
     ppi = penguinPiC.PenguinPi()
     keyboard_control = Keyboard(ppi)
-    cv2.namedWindow('video', cv2.WINDOW_NORMAL);
-    cv2.setWindowProperty('video', cv2.WND_PROP_AUTOSIZE, cv2.WINDOW_AUTOSIZE);
+    
+    cv2.namedWindow('PenguinPi Stream', cv2.WINDOW_AUTOSIZE);
 
     while True:
-        # font display options
+        # Font display options
         font = cv2.FONT_HERSHEY_SIMPLEX
-        location = (0, 0)
-        font_scale = 1
-        font_col = (255, 0, 0)
-        line_type = 2
+        font_scale = 0.8
+        font_col = (255, 127, 127)
+        line_type = 1
 
-        # get velocity of each wheel
-        wheel_vels = keyboard_control.latest_drive_signal();
-        L_Wvel = wheel_vels[0]
-        R_Wvel = wheel_vels[1]
+        left_wheel_vel, right_wheel_vel = keyboard_control.latest_drive_signal();
 
-        # get current camera frame
-        curr = ppi.get_image()
-
-        # scale to 144p
-        # feel free to change the resolution
-        resized = cv2.resize(curr, (960, 720), interpolation = cv2.INTER_AREA)
-
-        # feel free to add more GUI texts
-        cv2.putText(resized, 'PenguinPi', (15, 50), font, font_scale, font_col, line_type)
-        cv2.putText(resized, 'L Vel = '+str(L_Wvel)+', R Vel = '+str(R_Wvel), (15,100), font, font_scale, font_col, line_type)
-
-        cv2.imshow('video', resized)
+        # Get current camera frame
+        frame = ppi.get_image()
+        
+        # Scale to 144p
+        scale = 1.25
+        width = int(scale * frame.shape[1])
+        height = int(scale * frame.shape[0])
+        resized_frame = cv2.resize(frame, (width, height), interpolation = cv2.INTER_AREA)
+        
+        # OpenCV display
+        x = 15
+        y = 30
+        cv2.putText(resized_frame, 'PenguinPi', (x, y), font, font_scale, font_col, line_type)
+        cv2.putText(resized_frame, 'L_VEL = '+str(left_wheel_vel)+', R_VEL = '+str(right_wheel_vel), (15, 2*y), font, font_scale, font_col, line_type)
+        cv2.imshow('PenguinPi Stream', resized_frame)
         cv2.waitKey(1)
 
         continue
