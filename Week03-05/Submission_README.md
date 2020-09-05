59ecb6750a996d405d06874e337930e3026701ac
Maitri-J <mjus0002@student.monash.edu>
2020-09-06 08:18:57 +1000

    readme update

--- Week03-05/Submission_README.md
+++ Week03-05/Submission_README.md
@@ -1,1 +1,20 @@
 # How to run our robot
+
+- Make sure to update penguinpi.sdf in catkin_ws/src/penguinpi_description/urdf/penguinpi.sdf
+	(some changes to the friction value)
+- After setting up the environment, open a new terminal and run python3 manualSLAM.py
+- Press arrow keys to control the robot 
+
+Highlights for running the robot in another environment:
+	1. If there are any marker on sight by rotating, rotate a couple of times before moving on to the next marker.
+	2. Make sure to align the predicted arrow with the actual direction the robot is facing (by pressing A and D back and forth until it matches) before scanning a marker.
+
+Recommended path for full mark in penguinpi_arena (all within 1.5m range):
+	- Wheel_calibration values: 3.6s and 27.7 seconds
+	1. From start position, scan left and right around three times to get an accurate position of the first 2 markers. 
+	2. After scanning, facing slightly to the right of the sheep (so we don't collide) go towards the thirds marker and the robot will scan the third marker.
+	3. Steer around 90 degree to the left, towards the 4th marker. Rotate if needed to scan the marker. 
+	4. Steer around 90 degree to the left, towards the 5th marker. Rotate if needed to scan the marker. 
+	5. Scan left and right to make sure the direction of the arrow is still aligned with the robot. Then go to the last marker and scan.
+
+
---
+++ Week03-05/slam/wheel_calibration.py
@@ -0,0 +1,108 @@
+import numpy as np
+import os
+import sys
+
+import penguinPiC
+ppi = penguinPiC.PenguinPi()
+
+
+def calibrateWheelRadius():
+    # Compute the robot scale parameter using a range of wheel velocities.
+    # For each wheel velocity, the robot scale parameter can be computed
+    # by comparing the time and distance driven to the input wheel velocities.
+
+    # Feel free to change the range / step
+    wheel_velocities_range = range(20, 80, 15)
+    delta_times = []
+
+    for wheel_vel in wheel_velocities_range:
+        print("Driving at {} ticks/s.".format(wheel_vel))
+
+        # Repeat the test until the correct time is found.
+        while True:
+            delta_time = input("Input the time to drive in seconds: ")
+            try:
+                delta_time = float(delta_time)
+            except ValueError:
+                print("Time must be a number.")
+                continue
+
+            # Drive the robot at the given speed for the given time
+            ppi.set_velocity(wheel_vel, wheel_vel, delta_time)
+
+            uInput = input("Did the robot travel 1m?[y/N]")
+            if uInput == 'y':
+                delta_times.append(delta_time)
+                print("Recording that the robot drove 1m in {:.2f} seconds at wheel speed {}.\n".format(delta_time,
+                                                                                                        wheel_vel))
+                break
+
+    # Once finished driving, compute the scale parameter by averaging
+    num = len(wheel_velocities_range)
+    scale = 0
+    for delta_time, wheel_vel in zip(delta_times, wheel_velocities_range):
+        scale += 1 / num * (1 / (wheel_vel * delta_time))
+    print("The scale parameter is estimated as {:.2f} m/ticks.".format(scale))
+
+    return scale
+
+
+def calibrateBaseline(scale):
+    # Compute the robot basline parameter using a range of wheel velocities.
+    # For each wheel velocity, the robot baseline parameter can be computed by
+    # comparing the time elapsed and rotation completed to the input wheel
+    # velocities to find out the distance between the wheels.
+
+    # Feel free to change the range / step
+    wheel_velocities_range = range(30, 60, 10)
+    delta_times = []
+
+    for wheel_vel in wheel_velocities_range:
+        print("Driving at {} ticks/s.".format(wheel_vel))
+
+        # Repeat the test until the correct time is found.
+        while True:
+            delta_time = input("Input the time to drive in seconds: ")
+            try:
+                delta_time = float(delta_time)
+            except ValueError:
+                print("Time must be a number.")
+                continue
+
+            # Spin the robot at the given speed for the given time
+            ppi.set_velocity(-wheel_vel, wheel_vel, delta_time)
+
+            uInput = input("Did the robot spin 360deg?[y/N]")
+            if uInput == 'y':
+                delta_times.append(delta_time)
+                print("Recording that the robot spun 360deg in {:.2f} seconds at wheel speed {}.\n".format(delta_time,
+                                                                                                           wheel_vel))
+                break
+
+    # Once finished driving, compute the basline parameter by averaging
+    num = len(wheel_velocities_range)
+    baseline = 0
+    # TODO: compute baseline parameter
+    # ------------------------------------------
+    # ----------- Add your code here -----------
+    # ------------------------------------------
+    print("The baseline parameter is estimated as {:.2f} m.".format(baseline))
+
+    return baseline
+
+
+if __name__ == "__main__":
+    # calibrate pibot scale and baseline
+    dataDir = "{}/wheel_calibration/".format(os.getcwd())
+
+    print('Calibrating PiBot scale...\n')
+    scale = calibrateWheelRadius()
+    fileNameS = "{}scale.txt".format(dataDir)
+    np.savetxt(fileNameS, np.array([scale]), delimiter=',')
+
+    print('Calibrating PiBot baseline...\n')
+    baseline = calibrateBaseline(scale)
+    fileNameB = "{}baseline.txt".format(dataDir)
+    np.savetxt(fileNameB, np.array([baseline]), delimiter=',')
+
+    print('Finished calibration')
