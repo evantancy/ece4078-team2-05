62612fcf7caa924f48c1c8503940395f9027c8a9
Evan Tan <tcyevan@gmail.com>
2020-10-24 16:13:04 +1100

    ARUCO detect with camera matrix and distortion coeffs

--- Lab02_M5_Group2_05/slam/aruco_detector.py
+++ Lab02_M5_Group2_05/slam/aruco_detector.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import slam.Measurements as Measurements
 
+
 class aruco_detector:
     def __init__(self, robot, marker_length=0.1):
         self.camera_matrix = robot.camera_matrix
@@ -13,31 +14,37 @@ class aruco_detector:
 
     def detect_marker_positions(self, img):
         # Perform detection
-        corners, ids, rejected = cv2.aruco.detectMarkers(img, self.aruco_dict, parameters=self.aruco_params)
-        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, self.marker_length, self.camera_matrix, self.distortion_params)
-        id_flag = []
-        # print(tvecs)
-        # print(tvecs.shape)
+        corners, ids, rejected = cv2.aruco.detectMarkers(
+            img,
+            self.aruco_dict,
+            parameters=self.aruco_params,
+            cameraMatrix=self.camera_matrix,
+            distCoeff=self.distortion_params,
+        )
 
         if ids is None:
             return [], img
 
+        # Marker length should be scaled to pixel size
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(
+            corners, self.marker_length, self.camera_matrix, self.distortion_params
+        )
+
+        id_flag = []
+        MAX_DISTANCE = 15
+
         if tvecs is not None:
             for i in range(tvecs.shape[0]):
-                # print(tvecs[i][0])
                 total = np.linalg.norm(tvecs[i][0])
-                print(total)
-                if total > 15:
+                if total > MAX_DISTANCE:
                     id_flag.append(ids[i][0])
-
-
 
         # Compute the marker positions
         measurements = []
         seen_ids = []
         for i in range(len(ids)):
-            idi = ids[i,0]
             # Some markers appear multiple times but should only be handled once.
+            idi = ids[i, 0]
 
             if np.any(id_flag == idi):
                 continue
@@ -47,15 +54,20 @@ class aruco_detector:
                 else:
                     seen_ids.append(idi)
 
-                lm_tvecs = tvecs[ids==idi].T
-                lm_bff2d = np.block([[lm_tvecs[2,:]],[-lm_tvecs[0,:]]])
-                lm_bff2d = np.mean(lm_bff2d, axis=1).reshape(-1,1)
+                lm_tvecs = tvecs[ids == idi].T
+                lm_bff2d = np.block([[lm_tvecs[2, :]], [-lm_tvecs[0, :]]])
+                lm_bff2d = np.mean(lm_bff2d, axis=1).reshape(-1, 1)
 
+                # Covariance not specified
                 lm_measurement = Measurements.MarkerMeasurement(lm_bff2d, idi)
                 measurements.append(lm_measurement)
 
-        # Draw markers on image copy
-        img_marked = img.copy()
-        cv2.aruco.drawDetectedMarkers(img_marked, corners, ids)
+        # img_marked = img.copy()
+        # Draw markers on original
+        PINK = (255, 51, 255)
+        cv2.aruco.drawDetectedMarkers(img, corners, ids, PINK)
+        # Draw markers on copy
 
-        return measurements, img_marked
+        # cv2.aruco.drawDetectedMarkers(img_marked, corners, ids)
+
+        return measurements, img
