a77f75462d05d2545b73baf823b239877c775304
Evan Tan <tcyevan@gmail.com>
2020-11-10 16:47:50 +1100

    Update gitignore, changed variable name

--- Lab02_final_Group2_05/slam/Measurements.py
+++ Lab02_final_Group2_05/slam/Measurements.py
@@ -3,10 +3,10 @@ import numpy as np
 
 class MarkerMeasurement:
     # Measurements are of landmarks in 2D and have a position as well as tag id.
-    def __init__(self, position, tag, covariance=(0.2 * np.eye(2))):
+    def __init__(self, position, tag, initial_covariance=(0.2 * np.eye(2))):
         self.position = position
         self.tag = tag
-        self.covariance = covariance
+        self.covariance = initial_covariance
 
 
 class DriveMeasurement:
